6ed5a47deef05d28a0073c53026c46d0c71f9977
Michal Bilanin <xbilanin@fi.muni.cz>
2024-10-13 18:00:05 +0200

    feat: add dev container setup

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -11,8 +11,17 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContextFactory<JuiceWorldDbContext>(options =>
 {
-    IConfiguration configuration = builder.Configuration;
-    var connectionString = configuration.GetConnectionString("JuiceWorldDb");
+    const string connectionStringKey = "DB_CONNECTION_STRING";
+    var connectionString = Environment.GetEnvironmentVariable(connectionStringKey);
+
+    if (connectionString == null)
+    {
+        System.Diagnostics.Debug.Fail(
+            $"Connection string is null, make sure it is specified " +
+            $"in the environment variable: {connectionStringKey}");
+        return;
+    }
+
     options
         .UseNpgsql(connectionString)
         .LogTo(s => System.Diagnostics.Debug.WriteLine(s))
@@ -20,6 +29,18 @@ builder.Services.AddDbContextFactory<JuiceWorldDbContext>(options =>
 });
 
 var app = builder.Build();
+
+const string apiPortKey = "API_PORT";
+var apiPort = Environment.GetEnvironmentVariable(apiPortKey);
+if (apiPort == null)
+{
+    System.Diagnostics.Debug.Fail(
+        $"API port is null, make sure it is specified " +
+        $"in the environment variable: {apiPortKey}");
+    return;
+}
+
+app.Urls.Add($"https://localhost:{apiPort}");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
