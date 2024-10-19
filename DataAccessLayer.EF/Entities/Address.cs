fc0509baa314764fee01f98c4b1c95febb83669b
Michal Bilanin <xbilanin@fi.muni.cz>
2024-10-19 15:10:48 +0200

    feat: rename DataAccessLayer -> DataAccessLayer.EF

---
+++ DataAccessLayer.EF/Entities/Address.cs
@@ -0,0 +1,17 @@
+﻿using JuiceWorld.Enums;
+
+namespace JuiceWorld.Entities;
+
+public class Address : BaseEntity
+{
+    public string Name { get; set; }
+    public string City { get; set; }
+    public string Street { get; set; }
+    public string HouseNumber { get; set; }
+    public string ZipCode { get; set; }
+    public string Country { get; set; }
+    public AddressType Type { get; set; } = AddressType.Shipping;
+    public int UserId { get; set; }
+    public virtual User User { get; set; }
+    public virtual IEnumerable<Order> Orders { get; set; }
+}
