87f9eddab40d20b96d3dfc3792f7ad13d1108131
Michal Bilanin <xbilanin@fi.muni.cz>
2024-10-19 15:36:07 +0200

    feat: add repository implementation

---
+++ DataAccessLayer.EF/Repositories/Repository.cs
@@ -0,0 +1,50 @@
+using Infrastructure.Repositories;
+using JuiceWorld.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace JuiceWorld.Repositories;
+
+public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+{
+    private readonly JuiceWorldDbContext _context;
+    private readonly DbSet<TEntity> _dbSet;
+
+    public Repository(JuiceWorldDbContext context)
+    {
+        _context = context;
+        _dbSet = _context.Set<TEntity>();
+    }
+
+    public async Task<TEntity> Create(TEntity entity)
+    {
+        var result = await _dbSet.AddAsync(entity);
+        await _context.SaveChangesAsync();
+        return result.Entity;
+    }
+
+    public async Task<TEntity?> GetById(object id)
+    {
+        return await _dbSet.FindAsync(id);
+    }
+
+    public async Task<TEntity> Update(TEntity entity)
+    {
+        _dbSet.Attach(entity);
+        _context.Entry(entity).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+        return entity;
+    }
+
+    public async Task<bool> Delete(object id)
+    {
+        var entity = await _dbSet.FindAsync(id);
+        if (entity == null)
+        {
+            return false;
+        }
+
+        _dbSet.Remove(entity);
+        await _context.SaveChangesAsync();
+        return true;
+    }
+}
