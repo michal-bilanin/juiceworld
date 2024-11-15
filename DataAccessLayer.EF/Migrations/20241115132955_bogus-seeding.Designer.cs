49b7bb9990d8c72faf0f72c80c043a74cdfb8d17
Michal Bilanin <xbilanin@fi.muni.cz>
2024-11-15 14:33:29 +0100

    feat: seed data using bogus

--- DataAccessLayer.EF/Migrations/20241115132955_bogus-seeding.Designer.cs
+++ DataAccessLayer.EF/Migrations/20241115132955_bogus-seeding.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using JuiceWorld.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace JuiceWorld.Migrations
 {
     [DbContext(typeof(JuiceWorldDbContext))]
-    partial class JuiceWorldDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241115132955_bogus-seeding")]
+    partial class bogusseeding
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
