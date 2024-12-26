6ae6c4a15a1a8a9512adf27e505bb400cae59908
Michal Bilanin <xbilanin@fi.muni.cz>
2024-12-26 23:52:25 +0100

    fix: unexpected token

--- BusinessLayer/DTOs/ProductDetailDto.cs
+++ BusinessLayer/DTOs/ProductDetailDto.cs
@@ -12,7 +12,7 @@ public class ProductDetailDto
     public ProductCategory Category { get; set; }
     public ProductUsageType UsageType { get; set; }
 
-    public required ManufacturerDto Manufacturer { get; set; };
+    public required ManufacturerDto Manufacturer { get; set; }
     public IEnumerable<ReviewDto> Reviews { get; set; } = [];
     public IEnumerable<TagDto> Tags { get; set; } = [];
 }
