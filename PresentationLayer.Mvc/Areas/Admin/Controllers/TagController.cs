57de5497af461a4037eba8158d013a4e43585b64
Michal Bilanin <xbilanin@fi.muni.cz>
2024-12-26 15:19:48 +0100

    feat: add tag admin area

---
+++ PresentationLayer.Mvc/Areas/Admin/Controllers/TagController.cs
@@ -0,0 +1,82 @@
+using BusinessLayer.DTOs;
+using BusinessLayer.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using PresentationLayer.Mvc.ActionFilters;
+
+namespace PresentationLayer.Mvc.Areas.Admin.Controllers;
+
+[Area(Constants.Areas.Admin)]
+[RedirectIfNotAdminActionFilter]
+public class TagController(ITagService tagService) : Controller
+{
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var tags = await tagService.GetAllTagsAsync();
+        return View(tags);
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View(new TagDto { Name = "" });
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(TagDto viewModel)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(viewModel);
+        }
+
+        var createdTag = await tagService.CreateTagAsync(viewModel);
+        if (createdTag == null)
+        {
+            return BadRequest();
+        }
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Edit(int id)
+    {
+        var tag = await tagService.GetTagByIdAsync(id);
+        if (tag == null)
+        {
+            return NotFound();
+        }
+
+        return View(tag);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(TagDto viewModel)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(viewModel);
+        }
+
+        var updatedTag = await tagService.UpdateTagAsync(viewModel);
+        if (updatedTag == null)
+        {
+            return BadRequest();
+        }
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var isDeleted = await tagService.DeleteTagByIdAsync(id);
+        if (!isDeleted)
+        {
+            return BadRequest();
+        }
+
+        return RedirectToAction(nameof(Index));
+    }
+}
