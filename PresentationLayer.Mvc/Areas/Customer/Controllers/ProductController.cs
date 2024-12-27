d34de47d0d45df1df245f1a158d4b93e26610ece
Michal Bilanin <xbilanin@fi.muni.cz>
2024-12-27 19:16:11 +0100

    feat: products home page

--- PresentationLayer.Mvc/Areas/Customer/Controllers/ProductController.cs
+++ PresentationLayer.Mvc/Areas/Customer/Controllers/ProductController.cs
@@ -1,16 +1,100 @@
+using System.Security.Claims;
 using BusinessLayer.DTOs;
 using BusinessLayer.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using PresentationLayer.Mvc.ActionFilters;
 
 namespace PresentationLayer.Mvc.Areas.Customer.Controllers;
 
 [Area(Constants.Areas.Customer)]
-public class ProductController(IProductService productService) : Controller
+public class ProductController(IProductService productService, ICartItemService cartItemService, IReviewService reviewService) : Controller
 {
     [HttpGet]
     public async Task<IActionResult> Index([FromQuery] ProductFilterDto productFilter)
     {
         var products = await productService.GetProductDetailsFilteredAsync(productFilter);
         return View(products);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Details(int id)
+    {
+        var product = await productService.GetProductDetailByIdAsync(id);
+        if (product is null)
+        {
+            return NotFound();
+        }
+
+        return View(product);
+    }
+
+    [HttpPost]
+    [RedirectIfNotAuthenticatedActionFilter]
+    public async Task<IActionResult> AddToCart(AddToCartDto addToCartDto)
+    {
+        if (!int.TryParse(User.FindFirstValue(ClaimTypes.Sid) ?? string.Empty, out var userId))
+        {
+            return Unauthorized();
+        }
+
+        var success = await cartItemService.AddToCartAsync(addToCartDto, userId);
+        if (!success)
+        {
+            return BadRequest();
+        }
+
+        return Ok();
+    }
+
+    [HttpPost]
+    [RedirectIfNotAuthenticatedActionFilter]
+    public async Task<IActionResult> AddReview(ReviewDto reviewDto)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest();
+        }
+
+        if (!int.TryParse(User.FindFirstValue(ClaimTypes.Sid) ?? string.Empty, out var userId))
+        {
+            return Unauthorized();
+        }
+
+        if (reviewDto.UserId != userId)
+        {
+            return Unauthorized();
+        }
+
+        var review = await reviewService.CreateReviewAsync(reviewDto);
+        if (review is null)
+        {
+            return BadRequest();
+        }
+
+        return RedirectToAction(nameof(Details), new { id = review.ProductId });
+    }
+
+    [HttpPost]
+    [RedirectIfNotAuthenticatedActionFilter]
+    public async Task<IActionResult> DeleteReview(int id)
+    {
+        if (!int.TryParse(User.FindFirstValue(ClaimTypes.Sid) ?? string.Empty, out var userId))
+        {
+            return Unauthorized();
+        }
+
+        var review = await reviewService.GetReviewByIdAsync(id);
+        if (review is null || (review.UserId != userId && !User.IsInRole(Commons.Enums.UserRole.Admin.ToString())))
+        {
+            return Unauthorized();
+        }
+
+        var success = await reviewService.DeleteReviewByIdAsync(id);
+        if (!success)
+        {
+            return BadRequest();
+        }
+
+        return Ok();
     }
 }
